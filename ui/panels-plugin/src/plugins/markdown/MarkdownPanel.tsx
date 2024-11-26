e4c8ce8792b96d4386449f8aa8c2ac6085747afe
ZonaHe <zonahe@qq.com>
2024-11-26 15:56:53 +0800

    docs: add notice

--- ui/panels-plugin/src/plugins/markdown/MarkdownPanel.tsx
+++ ui/panels-plugin/src/plugins/markdown/MarkdownPanel.tsx
@@ -11,6 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This file is edited by ZonaHex in https://github.com/ZonaHex/perses for Greptime.
+
 import { useMemo } from 'react';
 import DOMPurify from 'dompurify';
 import { marked } from 'marked';
